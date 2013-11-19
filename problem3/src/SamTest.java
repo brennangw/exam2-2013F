43ec1d8b086908d091f7582446cae4d24ba63db2
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2013-11-18 22:31:00 -0600

    Add basic Unit Test framework

---
+++ problem3/src/SamTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * SamR's tests for this problem.  (Forthcoming.)
+ */
+public class SamTest {
+
+    @Test
+    public void test() {
+    } // test()
+} // SamTest
